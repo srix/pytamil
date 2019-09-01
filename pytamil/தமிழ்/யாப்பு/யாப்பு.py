5fcd6cbb5cb25a0815575300b2397aec83d54b5c
Srix <codeporukki@gmail.com>
2019-09-01 20:57:25 +0530

    adding யாப்பு module.

---
+++ pytamil/தமிழ்/யாப்பு/யாப்பு.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import tatsu
+from tatsu.ast import AST
+from pprint import pprint
+from codecs import open
+import os
+
+
+
+def load_parser(filename):
+    grammar = open(filename).read()
+
+    parser = tatsu.compile(grammar,parseinfo=True)
+    return parser
+    # ast = parser.parse(
+    #     '3 + 5 + ( 10 - 20 )',
+    #     semantics=CalcSemantics()
+    # )
+
+    # ast = parser.parse('நிலைமொழி|உடம்படுமெய்(ய்)|இரட்டுதல் + திரிதல்|வருமொழி ,நிலைமொழி|உடம்படுமெய்(வ்) + திரிதல்|வருமொழி' ,\
+    #                      trace = True, colorize =True)
+
+parser = load_parser(os.path.join(os.path.dirname(__file__),'குறள் வெண்பா.ebnf'))
+# entry = 'இல்இல் இல்இல் இல்இல் இல்இல் இல்இல் இல்இல் இல்'
+entry = '''உடுக்கை இழந்தவன் கைபோல ஆங்கே
+இடுக்கண் களைவதாம் நட்பு'''
+
+ast = parser.parse(entry, parseinfo=True, nameguard=False, trace = True, colorize =True)
+pprint(ast, width=20, indent=4)
