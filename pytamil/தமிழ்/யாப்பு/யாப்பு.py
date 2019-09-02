fb002ea378df6db4076f14ad34722aaa4ebab481
Srix <codeporukki@gmail.com>
2019-09-02 12:12:07 +0530

    made some changes to kural venba ebnf. the parser output is not correct yet. added ceer boundaries to ebnf

--- pytamil/தமிழ்/யாப்பு/யாப்பு.py
+++ pytamil/தமிழ்/யாப்பு/யாப்பு.py
@@ -1,9 +1,83 @@
 # -*- coding: utf-8 -*-
 import tatsu
 from tatsu.ast import AST
+from tatsu.util import asjson
 from pprint import pprint
 from codecs import open
 import os
+import regex
+import json
+
+
+class DrawGraph(object):
+    def வெண்பா(self, ast):
+        print ('வெண்பா')
+        return ast
+
+    def அடி(self, ast):
+        print ('அடி')
+        return ast
+    
+    def ஈற்றடி(self, ast):
+        print ('ஈற்றடி')
+        return ast
+
+    def சீர்(self, ast):
+        print ('சீர்')
+        return ast
+
+    def ஈற்றுச்சீர்(self, ast):
+        print ('ஈற்றுச்சீர்')
+        return ast 
+
+    def ஈரசை(self, ast):
+        print ('ஈரசை')
+        return ast
+
+    def மூவசை(self, ast):
+        print ('மூவசை')
+        return ast
+
+    def தேமா(self, ast):
+        print ('தேமா')
+        return ast
+
+    def புளிமா(self, ast):
+        print ('புளிமா')
+        return ast
+
+    def கருவிளம்(self, ast):
+        print ('கருவிளம்')
+        return ast
+
+    def கூவிளம்(self, ast):
+        print ('கூவிளம்')
+        return ast
+    
+    def தேமாங்காய்(self, ast):
+        print ('தேமாங்காய்')
+        return ast
+
+    def புளிமாங்காய்(self, ast):
+        print ('புளிமாங்காய்')
+        return ast
+
+    def கருவிளங்காய்(self, ast):
+        print ('கருவிளங்காய்')
+        return ast
+
+    def கூவிளங்காய்(self, ast):
+        print ('கூவிளங்காய்')
+        return ast   
+   
+    def நேர்(self, ast):
+        print ('நேர்')
+        return ast
+
+    def நிரை(self, ast):
+        print ('நிரை')
+        return ast   
+    
 
 
 
@@ -12,19 +86,38 @@ def load_parser(filename):
 
     parser = tatsu.compile(grammar,parseinfo=True)
     return parser
-    # ast = parser.parse(
-    #     '3 + 5 + ( 10 - 20 )',
-    #     semantics=CalcSemantics()
-    # )
 
-    # ast = parser.parse('நிலைமொழி|உடம்படுமெய்(ய்)|இரட்டுதல் + திரிதல்|வருமொழி ,நிலைமொழி|உடம்படுமெய்(வ்) + திரிதல்|வருமொழி' ,\
-    #                      trace = True, colorize =True)
 
-parser = load_parser(os.path.join(os.path.dirname(__file__),'குறள் வெண்பா.ebnf'))
-# entry = 'இல்இல் இல்இல் இல்இல் இல்இல் இல்இல் இல்இல் இல்'
-entry = '''உடுக்கைஇழந்தவன்கைபோலஆங்கேஇடுக்கண்களைவதாம்நட்பு'''
 
-ast = parser.parse(entry, parseinfo=True, nameguard=False,  
-                    whitespace='\t ',
-                    trace = True, colorize =True)
-pprint(ast, width=20, indent=3 )
+def main():
+    parser = load_parser(os.path.join(os.path.dirname(__file__),'குறள் வெண்பா.ebnf'))
+
+    entry = [
+    '''உடுக்கை இழந்தவன் கைபோல ஆங்கே
+    இடுக்கண் களைவதாம் நட்பு''',
+
+    '''கற்றதனால் ஆய பயனென்கொல் வாலறிவன்
+    நற்றாள் தொழாஅர் எனின்''']
+
+
+    text = entry[0]
+    #text = regex.sub('\s+','',entry[0]) # remove spaces and newline
+
+    # ast = parser.parse(text, 
+    #                     parseinfo=True, nameguard=False,  
+    #                     trace = True, colorize =True)
+
+    ast = parser.parse(text, semantics = DrawGraph(),
+                        nameguard=False,
+                       # trace = True, colorize =True
+                        )
+
+
+    pprint(ast, width=20, indent=4)
+
+    # print('JSON')
+    # print(json.dumps(asjson(ast), indent=2))
+    # print()
+  
+if __name__== "__main__":
+  main()
